cd73cf0baff3bacd2bae1ef3f7abfc36e0b0d50c
ggssh <2821764200@qq.com>
2021-12-30 15:18:57 +0800

    feat : 管理SV39多级页表

--- os/src/mm/mod.rs
+++ os/src/mm/mod.rs
@@ -1,7 +1,11 @@
 mod heap_allocator;
+mod address;
+mod page_table;
+mod frame_allocator;
 
 pub fn init(){
     heap_allocator::init_heap();
+    frame_allocator::init_frame_allocator();
 }
 
 pub fn mm_test_heap(){
